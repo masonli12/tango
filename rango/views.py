2d29dd922fb6163067e1bd9dfbad7d26dcd5ac2f
miao <mason201312@gmail.com>
2016-01-06 18:06:54 +0000

    ajax done

--- rango/views.py
+++ rango/views.py
@@ -245,3 +245,40 @@ def like_category(request):
             cat.likes = likes
             cat.save()
     return HttpResponse(likes)
+
+def get_category_list_start(max_results=0, starts_with=''):
+    cat_list = []
+    
+    if starts_with:
+        cat_list = Category.objects.filter(name__startswith=starts_with)
+
+    if max_results > 0:
+        if cat_list.count() > max_results:
+            cat_list = cat_list[:max_results]
+    return cat_list
+
+def suggest_category(request):
+    cat_list = []
+    starts_with = ''
+    if request.method == 'GET':
+        starts_with = request.GET['suggestion']
+    cat_list = get_category_list_start(8, starts_with)
+
+    return render(request, 'rango/cats.html', {'cats': cat_list })
+
+@login_required
+def auto_add_page(request):
+    catid = None
+    title = None
+    url = None
+    context_dict = {}
+    if request.method == "GET":
+        catid = request.GET['category_id']
+        url = request.GET['url']
+        title = request.GET['title']
+        if catid:
+            cat = Category.objects.get(id=int(catid))
+            p = Page.objects.get_or_create(category=cat, title=title, url=url)
+            pages = Page.objects.filter(category=cat).order_by('-views')
+            context_dict['pages'] = pages
+    return render(request, 'rango/page_list.html', context_dict)
